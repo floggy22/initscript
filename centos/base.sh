7ba2059106092ef9f6d1882491430fc7db5dbb4c
floggy22 <floggy22@googlemail.com>
2020-06-27 01:00:01 +0200

    Update base.sh

--- centos/base.sh
+++ centos/base.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
 echo "Update"
-sudo yum -y update
+sudo dnf -y update
 
+echo "Ansible"
+sudo dnf makecache
+sudo dnf -y install epel-release
+sudo dnf makecache
+sudo dnf -y install ansible
+
+
+echo "VirtualBox"
+sudo dnf -y install gcc kernel-devel kernel-headers dkms make bzip2 perl
 wget http://download.virtualbox.org/virtualbox/6.1.8/VBoxGuestAdditions_6.1.8.iso
+sudo mount -t iso9660 -o loop VBoxGuestAdditions_6.1.8.iso /media/
+
+cd /media
+sudo sh ./VBoxLinuxAdditions.run --nox11
+
+ssh-keygen -b 2048 -t rsa -q -N ""
