65c4ea93297c28853375fbd3c4f34e82fdacdd84
Martin <floggy22@googlemail.com>
2020-06-27 08:04:51 +0200

    changes and add cos7

---
+++ centos/base7.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+vbv=6.1.10
+
+sudo systemctl enable --now cockpit.socket
+sudo firewall-cmd --permanent --add-service=cockpit
+sudo firewall-cmd --reload
+
+echo "Update"
+sudo yum -y update
+
+echo "Ansible"
+sudo yum -y install epel-release
+sudo yum -y install ansible
+
+echo "VirtualBox"
+sudo yum -y install gcc kernel-devel kernel-headers dkms make bzip2 perl
+
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$vbv/VBoxGuestAdditions_$vbv.iso
+sudo mount -t iso9660 -o loop VBoxGuestAdditions_$vbv.iso /media/
+
+cd /media
+sudo sh ./VBoxLinuxAdditions.run
+cd /tmp
+sudo umount /media
+rm VBoxGuestAdditions_$vbv.iso
+
+#ssh-keygen -b 2048 -t rsa -q -N ""
+
+cd ~
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+#sudo usermod -aG docker martin
+#sudo systemctl start docker
