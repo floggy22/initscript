eefeaa2346ee8c04c12de67b99765243b209b700
floggy22 <floggy22@googlemail.com>
2020-12-01 08:37:40 +0100

    old stuff

---
+++ centos/base7old.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+vbv=6.1.10
+
+echo "Update"
+sudo yum clean all
+sudo yum -y update
+
+sudo yum -y install cockpit
+sudo systemctl enable --now cockpit.socket
+sudo firewall-cmd --permanent --add-service=cockpit
+sudo firewall-cmd --reload
+
+
+echo "Ansible"
+sudo yum -y install epel-release
+sudo yum -y install ansible
+
+echo "VirtualBox"
+sudo yum -y install gcc kernel-devel kernel-headers dkms make bzip2 perl
+
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$vbv/VBoxGuestAdditions_$vbv.iso
+sudo mount -t iso9660 -o loop VBoxGuestAdditions_$vbv.iso /media/
+
+cd /media
+sudo sh ./VBoxLinuxAdditions.run
+cd /tmp
+sudo umount /media
+rm VBoxGuestAdditions_$vbv.iso
+
+ssh-keygen -b 4096 -t rsa -q -N ""
+
+cd ~
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+#sudo usermod -aG docker martin
+#sudo systemctl start docker
