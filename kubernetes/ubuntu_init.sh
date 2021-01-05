0fe0841f6b165ec5905281ac05a054a0b1643613
floggy22 <floggy22@googlemail.com>
2021-01-05 22:50:20 +0100

    fixes

--- kubernetes/ubuntu_init.sh
+++ kubernetes/ubuntu_init.sh
@@ -16,9 +16,9 @@ apt update
 apt upgrade -y
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
-apt install -y docker-ce containerd.io
+apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal containerd.io
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
