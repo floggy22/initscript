f639d2ffd48391c2b7fa51c16b89acae738d2f37
floggy22 <floggy22@googlemail.com>
2021-01-09 10:47:43 +0100

    nfs parameter

--- labor/vagrantTestNFS/init_nfsserver.sh
+++ labor/vagrantTestNFS/init_nfsserver.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y nfs-kernel-server
+
+sudo mkdir -p /var/nfs/general
+sudo chown nobody:nogroup /var/nfs/general
+sudo chmod g+w a+w /var/nfs/general
+
+sudo mkdir -p /var/nfs/new
+sudo chown nobody:nogroup /var/nfs/new
+sudo chmod g+w a+w /var/nfs/new
+
+sudo echo "/var/nfs/general  172.16.16.0/24:(rw,nohide,insecure,sync,no_subtree_check)" >> /etc/exports
+sudo echo "/var/nfs/new  172.16.16.0/24:(rw,nohide,insecure,sync,no_subtree_check)" >> /etc/exports
