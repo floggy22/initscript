78d40a63a3a0e5abcdd691ee02c8c59761e37d43
floggy22 <floggy22@googlemail.com>
2021-01-09 20:47:42 +0100

    some changes

--- labor/vagrantTestNFS/init_nfsserver.sh
+++ labor/vagrantTestNFS/init_nfsserver.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-sudo apt update
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y nfs-kernel-server
 
 sudo mkdir -p /var/nfs/general
-sudo chown nobody:nogroup /var/nfs/general
+sudo chown nobody: /var/nfs/general
 sudo chmod a+w /var/nfs/general
 
 sudo mkdir -p /var/nfs/new
 sudo chown nobody:nogroup /var/nfs/new
 sudo chmod a+w /var/nfs/new
 
-sudo echo "/var/nfs/general  172.16.16.0/24(rw,nohide,insecure,sync,no_subtree_check)" >> /etc/exports
-sudo echo "/var/nfs/new  172.16.16.0/24(rw,nohide,insecure,sync,no_subtree_check)" >> /etc/exports
+sudo echo "/var/nfs/general *(rw,insecure,sync,no_subtree_check,no_root_squash,no_all_squash)" >> /etc/exports
+sudo echo "/var/nfs/new *(rw,insecure,sync,no_subtree_check,no_root_squash,no_all_squash)" >> /etc/exports
 
 systemctl restart nfs-server
 showmount -e
+exportfs -v
