2ec8ae1a4716a9c3d7d56e5b10ab330694cfc7e8
floggy22 <floggy22@googlemail.com>
2021-01-09 18:32:11 +0100

    all in one run

--- kubernetes/vagrant/dashboard/init_m_kubedashboard.sh
+++ kubernetes/vagrant/dashboard/init_m_kubedashboard.sh
@@ -2,7 +2,7 @@
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 #kubectl create serviceaccount admin-dashboard -n kubernetes-dashboard
-#kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-dashboard | awk '{print $1}')
+kubectl apply -f /vagrant/dashboard/dashboard.yaml
 
 
-
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-dashboard | awk '{print $1}')
