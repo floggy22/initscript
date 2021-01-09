4c17a2ef20907c3d0af2182629f034c84cddfde4
floggy22 <floggy22@googlemail.com>
2021-01-09 18:12:23 +0100

    add dashboard with NodePort

---
+++ kubernetes/vagrant/dashboard/init_m_kubedashboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+#kubectl create serviceaccount admin-dashboard -n kubernetes-dashboard
+#kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-dashboard | awk '{print $1}')
+
+
+
