58c6f136d13a3eed5beb9c459d5f8ab98c545bfa
floggy22 <floggy22@googlemail.com>
2021-01-12 19:33:24 +0100

    bootstrap dns

--- labor/vagrantTestDNS/bootstrap.sh
+++ labor/vagrantTestDNS/bootstrap.sh
@@ -2,5 +2,16 @@
 
 sudo dnf update -y
 sudo dnf install -y bind bind-utils nano
-sudo firewall-cmd --permanent --add-port={53/udp,53/tcp}  
+sudo firewall-cmd --permanent --add-port={53/udp,53/tcp} 
+sudo firewall-cmd --permanent --add-port={953/udp,953/tcp}  
 sudo systemctl reload firewalld
+
+sudo rndc-confgen -a
+sudo chgrp named /etc/rndc.key
+sudo chmod g+r /etc/rndc.key
+
+sudo cp /vagrant/named.conf /etc/named.conf
+sudo cp /vagrant/named.zones /etc/named.zones
+
+sudo cp /vagrant/foward.labor /var/named/foward.labor
+sudo cp /vagrant/reverse /var/named/reverse.labor
