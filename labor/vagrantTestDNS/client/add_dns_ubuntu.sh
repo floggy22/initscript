8889fd1a6f10db2351aa223bb5ef55ec8b7cef27
floggy22 <floggy22@googlemail.com>
2021-01-16 19:29:42 +0100

    diff fixes

--- labor/vagrantTestDNS/client/add_dns_ubuntu.sh
+++ labor/vagrantTestDNS/client/add_dns_ubuntu.sh
@@ -2,7 +2,7 @@
 
 echo "Add DNS settings - ROOT"
 
-sudo cat /etc/netplan/50-vagrant.yaml /vagrant/client/netplan.yaml >> 60.myconfig.yaml
+sudo cat /etc/netplan/50-vagrant.yaml /vagrant/client/netplan.yaml >> /etc/netplan/60-myconfig.yaml
 sudo rm /etc/netplan/50-vagrant.yaml
 sudo netplan apply
 
