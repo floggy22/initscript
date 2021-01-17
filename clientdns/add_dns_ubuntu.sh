229d1d48840c4bf806c148660930d062f2176ad3
floggy22 <floggy22@googlemail.com>
2021-01-17 10:22:58 +0100

    run only root

--- clientdns/add_dns_ubuntu.sh
+++ clientdns/add_dns_ubuntu.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#only root
+
+ID=$(id -u)
+
+if [ "$ID" -ne 0 ]; then
+        echo "Runs as root or sudo"
+        echo "$ID"
+        exit 1
+else
+        echo "OK"
+        echo "$ID"
+fi
 
 SPATH=`dirname "$0"`
 
 echo "Add DNS settings - ROOT"
-
 sudo cat /etc/netplan/50-vagrant.yaml $SPATH/netplan.yaml >> /etc/netplan/60-myconfig.yaml
 sudo rm /etc/netplan/50-vagrant.yaml
 sudo netplan apply
