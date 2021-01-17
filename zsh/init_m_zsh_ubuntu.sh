d61537c59a5cce65a388fab60dd5bb7f151581e6
floggy22 <floggy22@googlemail.com>
2021-01-17 09:27:12 +0100

    dirty fix

--- zsh/init_m_zsh_ubuntu.sh
+++ zsh/init_m_zsh_ubuntu.sh
@@ -5,11 +5,12 @@ sudo apt update
 sudo apt -y install zsh powerline fonts-powerline zsh-syntax-highlighting
 
 sudo usermod -s /usr/bin/zsh $(whoami)
-cd ~
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 cp .p10k.zsh ~/.
 cp .zshrc ~/.
 chown $(id -u):$(id -g) ~/.p10k.zsh
 chown $(id -u):$(id -g) ~/.zshrc
+
+cd ~
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 zsh
