0febeb393a24ef7e37adca173434138145c4ba01
floggy22 <floggy22@googlemail.com>
2021-01-17 09:43:45 +0100

    improve

--- zsh/init_m_zsh_ubuntu.sh
+++ zsh/init_m_zsh_ubuntu.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+SPATH=`dirname $0`
+
 echo "Install zsh"
 sudo apt update
 sudo apt -y install zsh powerline fonts-powerline zsh-syntax-highlighting
 
 sudo usermod -s /usr/bin/zsh $(whoami)
-cp .p10k.zsh ~/.
-cp .zshrc ~/.
+cp $SPATH/.p10k.zsh ~/.
+cp $SPATH/.zshrc ~/.
 chown $(id -u):$(id -g) ~/.p10k.zsh
 chown $(id -u):$(id -g) ~/.zshrc
 
-cd ~
+pushd ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+popd
 
 zsh
