68aac4b5e2c0d47e116f72a0267c7fe312c3793a
floggy22 <floggy22@googlemail.com>
2021-01-23 17:33:47 +0100

    add debug output

--- clientdns/add_dns_ubuntu.sh
+++ clientdns/add_dns_ubuntu.sh
@@ -16,8 +16,8 @@ fi
 SPATH=`dirname "$0"`
 
 echo "Add DNS settings - ROOT"
-sudo cat /etc/netplan/50-vagrant.yaml $SPATH/netplan.yaml >> /etc/netplan/60-myconfig.yaml
+sudo cat /etc/netplan/50-vagrant.yaml $SPATH/netplan.yaml >> /etc/netplan/10-myconfig.yaml
 sudo rm /etc/netplan/50-vagrant.yaml
-sudo netplan apply
+sudo netplan -debug apply
 
 echo "EDIT /etc/hosts manual!!"
