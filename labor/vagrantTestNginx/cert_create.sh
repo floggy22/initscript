5c92ac4113c43c29df5762860f6a3b2b2f5e26cf
floggy22 <floggy22@googlemail.com>
2021-01-25 22:57:51 +0100

    tls with ingress

--- labor/vagrantTestNginx/cert_create.sh
+++ labor/vagrantTestNginx/cert_create.sh
@@ -9,3 +9,6 @@ openssl req -new -key labor.lan.key -out labor.lan.csr
 
 #Sign certificate with ca
 openssl x509 -req -in labor.lan.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out labor.lan.crt
+
+#convert crt to pem
+openssl x509 -in labor.lan.crt -out labor.lan.pem -outform PEM
