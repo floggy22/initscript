7176961435ca67f8c6e64c749613ed228ca86bf5
floggy22 <floggy22@googlemail.com>
2021-01-26 10:46:16 +0100

    test okd4-services

--- okd/vagrant/bootstrap_services.sh
+++ okd/vagrant/bootstrap_services.sh
@@ -12,12 +12,18 @@ systemctl enable --now xrxp
 firewall-cmd --zone=public --permanent --add-port=3389/tcp
 firewall-cmd --reload
 
-echo "[TASK 3] Firewall"
+
+echo "[TASK 3] HA Proxy and Apache"
+dnf install -y haproxy
+dnf install -y httpd
+
+echo "[TASK 4] Firewall AND SELinux"
 firewall-cmd --permanent --add-port=6443/tcp
 firewall-cmd --permanent --add-port=22623/tcp
 firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-service=https
+firewall-cmd --permanent --add-port=8080/tcp
 firewall-cmd --reload
+setsebool -P httpd_read_user_content 1
 
-echo "[TASK 4] HA Proxy"
-dnf install haproxy -y
+
