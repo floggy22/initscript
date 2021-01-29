74e443e1c20916c365bd2b60ffea085c3eb43aec
floggy22 <floggy22@googlemail.com>
2021-01-29 13:53:19 +0100

    fix path

--- clientdns/add_dns_centos.sh
+++ clientdns/add_dns_centos.sh
@@ -18,7 +18,7 @@ echo "Add DNS settings - ROOT"
 
 sed -i 's/^PEERDNS=no.*/PEERDNS=yes/' /etc/sysconfig/network-scripts/ifcfg-eth1
 sed -i 's/^PEERDNS=yes.*/PEERDNS=no/' /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "PEERDNS=no" >> etc/sysconfig/network-scripts/ifcfg-eth0
+echo "PEERDNS=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 
 echo "DNS1=172.16.16.5" >> /etc/sysconfig/network-scripts/ifcfg-eth1
 echo "DOMAIN=labor.lan" >> /etc/sysconfig/network-scripts/ifcfg-eth1
