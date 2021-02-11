b49f81fd8eb2a4fbaace32dda34b481a2a5643ff
floggy22 <floggy22@googlemail.com>
2021-02-11 10:35:31 +0100

    Update install.sh

--- pi/accesspoint/install.sh
+++ pi/accesspoint/install.sh
@@ -4,7 +4,7 @@ echo "install partly"
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install hostapd dnsmasq
+sudo apt install -y hostapd dnsmasq
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 
 sudo systemctl unmask hostapd
