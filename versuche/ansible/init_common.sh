7a8473fc3f7a612aa05ff2430a1c3a007138ebbe
floggy22 <floggy22@googlemail.com>
2021-03-08 14:57:52 +0100

    init new test

---
+++ versuche/ansible/init_common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "Clone init repo"
+cd ~
+git clone https://github.com/floggy22/initscript.git
+
+echo "Add dns"
+cd ~
+bash initscript/clientdns/add_dns_centos_en.sh
