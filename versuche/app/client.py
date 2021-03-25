cbb9d0a5cf64aa22b00a98a422232f833f716857
Martin <floggy22@googlemail.com>
2021-03-25 17:47:57 +0100

    stupid python stuff

--- versuche/app/client.py
+++ versuche/app/client.py
@@ -1,14 +1,25 @@
 #%%
-
 import socket
+import threading
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_addr = ('127.0.0.1', 1337)
-clientSocket.connect(server_addr)
+class client:
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def sendMsg(self):
+        while True:
+            self.clientSocket.send(bytes(input("")), 'utf-8')
 
+    def __init__(self):
+        server_addr = ('127.0.0.1', 1337)
+        self.clientSocket.connect(server_addr)
+        iThread = threading.Thread(target=self.sendMsg)
+        iThread.daemon = True
+        iThread.start
 
-for i in range(1,10):
-    clientSocket.send(bytes("Hi" + str(i), "utf8"))
+        while True:
+            data = self.clientSocket.recv(1024)
+            if not data:
+                break  
+            print(data)  
 
 
 # %%
--- versuche/app/server.py
+++ versuche/app/server.py
@@ -1,10 +1,33 @@
 import socket
+import threading
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_addr = ('127.0.0.1', 1337)
-#server_addr = (socket.gethostname(), 1337)
-serverSocket.bind(server_addr)
-serverSocket.listen(10)
-while True:
-    (clientSocket, addr) = serverSocket.accept()
-    print(clientSocket.recv(1024))
+class Server:
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connections = []
+
+    def __init__(self):
+        server_address = ('127.0.0.1', 1337)
+        self.serverSocket.bind(server_address)
+        self.serverSocket.listen(1)
+    
+    def handler(self, c, a):
+        while True:
+            data = c.recv(1024)
+            print(data)
+            for connection in self.connections:
+                connection.send(data)
+            if not data:
+                break
+
+    def run(self):
+        print("Run Server...wait for connections")
+        while True:
+            c, a = self.serverSocket.accept()
+            cThread = threading.Thread(target=self.handler, args=(c, a))
+            cThread.daemon = True
+            cThread.start()
+            self.connections.append(c)
+            print(self.connections)
+
+server = Server()
+server.run()
