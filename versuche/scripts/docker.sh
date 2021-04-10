3eb3b533012d7f69f270de3ba30a1b0632d31257
Martin <floggy22@googlemail.com>
2021-04-10 10:24:28 +0200

    add user to group

--- versuche/scripts/docker.sh
+++ versuche/scripts/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#run as sudo
+
 apt update
 apt upgrade -y
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -7,3 +9,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce containerd.io
+
+usermod -aG docker $USER
