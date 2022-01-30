1405473778bd2207a08b19713b0c2ad2d5763680
floggy22 <floggy22@googlemail.com>
2022-01-30 09:51:49 +0100

    simple kind testnode

---
+++ kubernetes/kind/bootstrap.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+## !IMPORTANT ##
+#
+## This script is tested only in the generic/ubuntu2004 Vagrant box
+## If you use a different version of Ubuntu or a different Ubuntu Vagrant box test this again
+#
+
+echo "[TASK 0] update and install"
+apt update
+apt upgrade -y
+apt install -y curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+groupadd docker
+usermod -aG docker vagrant
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "[TASK 3] Enable and Load Kernel modules"
+cat >>/etc/modules-load.d/containerd.conf<<EOF
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+
+echo "[TASK 4] Add Kernel settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+echo "[TASK 8] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 9] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+
+echo "[TASK 10] kind install"
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64 
+chmod +x ./kind
+mv ./kind /usr/bin/kind
