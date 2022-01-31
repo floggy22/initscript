a65f7351f54341af814716e90a288b1b4a0bf3ec
Martin Kessler <martin@lumagu.com>
2022-01-31 18:16:36 +0100

    time

--- kubernetes/kind/install_ingress.sh
+++ kubernetes/kind/install_ingress.sh
@@ -3,8 +3,8 @@
 echo "Init Ingress..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-echo "Wait...."
+echo "Wait ...."
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
-  --timeout=90s
+  --timeout=120s
